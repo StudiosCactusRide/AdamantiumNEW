641a58e7d7d4555b31169ee5c50d1846878f3f1b
StudiosCactusRide <studioscactusride@gmail.com>
2016-03-13 01:24:29 -0300

    V1

---
+++ SplashBall1/Assets/Scripts/BallBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallBehaviour : MonoBehaviour {
+
+	private int i;
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		i = Random.Range (-15, 15);
+		anim = GetComponentInChildren<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Rotate ();
+		SelfDestroy ();
+	}
+
+	void Rotate(){
+		
+		this.gameObject.transform.eulerAngles = new Vector3 (0f, 0f, transform.eulerAngles.y -i);
+		if (i > 0) {
+			i+=5;
+		} else {
+			i-=5;
+		}
+	}
+
+	void SelfDestroy(){
+		if (transform.position.y < -10) {
+			Debug.Log ("Tira uma vida do cara");
+			Destroy (gameObject);
+
+		}
+	}
+
+	void OnMouseDown(){
+		if (transform.tag == "Ball") {
+			anim.SetBool ("Shoot", true);
+			Debug.Log ("GG");
+			Destroy (gameObject, 0.5f);
+			//contar pontos
+		} else if (transform.tag == "Bomb") {
+			anim.SetBool ("Shoot", true);
+			Debug.Log ("Game over");
+			Destroy (gameObject, 0.5f);
+		}  
+	}
+
+
+}
---
+++ SplashBall1/Assets/Scripts/Mira.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mira : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0);
+	}
+	/*
+	public void OnCollisionEnter2D(Collision2D other){
+		if (other.gameObject.tag == "Ball") {
+			anim.SetBool ("Shoot", true);
+			Debug.Log ("GG");
+			//contar pontos
+		} else if (other.gameObject.tag == "Bomb") {
+			Debug.Log ("Game over");
+		}
+	}
+	*/	
+}
